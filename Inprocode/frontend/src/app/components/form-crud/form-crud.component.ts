5698d4ddcd363b0b88a203d0ca8150c9cab2c61d
raferto1973 <infowebs.rafa@gmail.com>
2024-03-11 21:32:42 +0100

    add formCrud component

---
+++ Inprocode/frontend/src/app/components/form-crud/form-crud.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-crud',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-crud.component.html',
+  styleUrl: './form-crud.component.scss'
+})
+export class FormCrudComponent {
+
+}
