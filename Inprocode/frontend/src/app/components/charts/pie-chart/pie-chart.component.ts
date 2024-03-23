ba1a0146bf749f4b961a3667fd6afddafff81dcf
raferto1973 <infowebs.rafa@gmail.com>
2024-03-23 18:48:08 +0100

    add pie chart

--- Inprocode/frontend/src/app/components/charts/pie-chart/pie-chart.component.ts
+++ Inprocode/frontend/src/app/components/charts/pie-chart/pie-chart.component.ts
@@ -1,14 +1,49 @@
 
 
 import { Component } from '@angular/core';
+import { ApexChart, ApexNonAxisChartSeries, ApexResponsive, NgApexchartsModule } from 'ng-apexcharts';
+
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+};
+
 
 @Component({
   selector: 'app-pie-chart',
   standalone: true,
-  imports: [ ],
+  imports: [ NgApexchartsModule ],
   templateUrl: './pie-chart.component.html',
   styleUrl: './pie-chart.component.scss'
 })
 export class PieChartComponent {
 
+  public chartOptions!: ChartOptions
+
+  constructor() {
+    this.chartOptions = {
+      series: [44, 55, 13, 43, 22],
+      chart: {
+        width: 380,
+        type: "pie"
+      },
+      labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+  }
 }
