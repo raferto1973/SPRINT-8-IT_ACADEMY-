b8349e35f6b0724561492e15d96aa4ea8b4a15d4
unknown <formacio@PC-1062590.formacio.local>
2024-04-03 10:20:43 +0200

    Global settings

--- Inprocode/frontend/src/environments/environment.ts
+++ Inprocode/frontend/src/environments/environment.ts
@@ -1,7 +1,6 @@
 
 
 export const environment = {
-  mapbox_key: "pk.eyJ1IjoicmFmZXJ0byIsImEiOiJjbHRyazFhc2IwaXc3MmtudjM0Z3JiaTh0In0.vuKh97HY1_RFLJOQxLSRxg",
   otra: "PROPIEDAD",
   production: false,
   endpoint: 'http://localhost:3000/'
