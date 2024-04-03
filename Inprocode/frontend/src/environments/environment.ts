69c30d5c1add6d00fe30d43a34e2353dcdf8982b
unknown <formacio@PC-1062590.formacio.local>
2024-04-03 10:19:01 +0200

    global settings

---
+++ Inprocode/frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+
+
+export const environment = {
+  mapbox_key: "pk.eyJ1IjoicmFmZXJ0byIsImEiOiJjbHRyazFhc2IwaXc3MmtudjM0Z3JiaTh0In0.vuKh97HY1_RFLJOQxLSRxg",
+  otra: "PROPIEDAD",
+  production: false,
+  endpoint: 'http://localhost:3000/'
+};
